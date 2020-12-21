0f6dc7c94eb7747aeba78525f1762d99edf8d5e3
John Wu <jwuphysics@gmail.com>
2020-12-21 11:26:22 -0500

    silly final newline...

--- day6/solution2.py
+++ day6/solution2.py
@@ -6,5 +6,6 @@ for answers in ''.join(lines).split('\n\n'):
     n = len(answers.split('\n'))
     for questions in set(''.join(answers.split('\n'))):
         if answers.count(questions) == n:
+            print(questions)
             count += 1
 print(count)
